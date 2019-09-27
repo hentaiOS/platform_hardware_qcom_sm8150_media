4c012213cb98a64512857cfc623ddef7931fe615
Dyneteve <sound0020@gmail.com>
2019-09-27 18:25:28 +0000

    media: Depend on display_headers not display_intf_headers
    Change-Id: I5f1649fc5e30c5efe5503d329284b4540e1f9768

--- mm-video-v4l2/vidc/venc/Android.mk
+++ mm-video-v4l2/vidc/venc/Android.mk
@@ -108,7 +108,7 @@ LOCAL_HEADER_LIBRARIES := \
         libcutils_headers \
         libutils_headers \
         libhardware_headers \
-        display_intf_headers
+        display_headers
 
 LOCAL_C_INCLUDES                := $(libmm-venc-inc)
 LOCAL_ADDITIONAL_DEPENDENCIES   := $(libmm-venc-add-dep)
@@ -155,7 +155,7 @@ LOCAL_HEADER_LIBRARIES := \
         libnativebase_headers \
         libutils_headers \
         libhardware_headers \
-        display_intf_headers
+        display_headers
 
 LOCAL_C_INCLUDES                := $(libmm-venc-inc)
 LOCAL_ADDITIONAL_DEPENDENCIES   := $(libmm-venc-add-dep)
